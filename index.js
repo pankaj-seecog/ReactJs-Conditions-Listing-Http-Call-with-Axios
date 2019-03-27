ff5467adb45d5a572881a378803d8f4568110a89
somethingnothingforsomething <pankaj.7613@gmail.com>
2019-03-27 18:32:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import Cities from './Cities';
+import Myblog from './Myblog';
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<Myblog />, document.getElementById('root'));
